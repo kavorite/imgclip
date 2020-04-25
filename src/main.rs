09d5c2ff1bb00db2ac15468408cb1228fc06f335
kavorite <iseurie@gmail.com>
2020-04-24 23:48:54 -0400

    output .bmp to disk on clipboard update

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ extern crate crossbeam_channel;
 extern crate png;
 extern crate reqwest;
 extern crate winapi;
+extern crate winrt_notification;
 
 mod dib;
 mod pin;
 
-use crossbeam_channel::TryRecvError;
 use dib::DIB;
 use pin::*;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
-use winapi::shared::minwindef::TRUE as WIN_TRUE;
+use winapi::um::wingdi::*;
 use winapi::um::winuser::*;
 
 fn main() -> std::io::Result<()> {
@@ -26,12 +25,49 @@ fn main() -> std::io::Result<()> {
                 if msg.msg != WM_CLIPBOARDUPDATE {
                     continue;
                 }
-                let mut clipboard = Clipboard::open()?;
-                if let Some(result) = DIB::get_clip(&mut clipboard) {
-                    let dib = result?;
-                    let ostrm = File::create("clip.png")?;
-                    dib.encode_png(ostrm)?;
+                let clipboard = Clipboard::open()?;
+                if let Some(gptr) = clipboard.get(CF_DIB) {
+                    let lock = gptr?;
+                    let info = lock.as_ref::<BITMAPINFO>();
+                    let file_info = &info.bmiHeader;
+                    let file_hdr = {
+                        let offset = {
+                            std::mem::size_of::<BITMAPFILEHEADER>()
+                                + std::mem::size_of::<BITMAPINFOHEADER>()
+                        } as u32;
+                        BITMAPFILEHEADER {
+                            bfType: 0x4D42,
+                            bfReserved1: 0,
+                            bfReserved2: 0,
+                            bfSize: file_info.biSizeImage + offset,
+                            bfOffBits: offset as u32,
+                        }
+                    };
+                    let mut ostrm = File::create("clip.bmp")?;
+                    println!("{:?}", file_hdr);
+                    println!("{:?}", file_info);
+                    ostrm.write({
+                        let ptr = <*const _>::cast(&file_hdr);
+                        let len = std::mem::size_of::<BITMAPFILEHEADER>();
+                        std::slice::from_raw_parts(ptr, len)
+                    })?;
+                    ostrm.write({
+                        let ptr = <*const _>::cast(file_info);
+                        let len = std::mem::size_of::<BITMAPINFOHEADER>();
+                        std::slice::from_raw_parts(ptr, len)
+                    })?;
+                    ostrm.write({
+                        let ptr = (file_info as *const BITMAPINFOHEADER).offset(1) as *const u8;
+                        let len = file_info.biSizeImage as usize;
+                        std::slice::from_raw_parts(ptr, len)
+                    })?;
                 }
+                // let mut clipboard = Clipboard::open()?;
+                // if let Some(result) = DIB::get_clip(&mut clipboard) {
+                //     let dib = result?;
+                //     let ostrm = File::create("clip.png")?;
+                //     dib.encode_png(ostrm)?;
+                // }
             }
             listener.poll()?;
         }
