4b4d292514211e0c0379f5602686e139aa07f777
kavorite <iseurie@gmail.com>
2020-04-25 00:19:13 -0400

    factor out dib codec

--- src/dib.rs
+++ src/dib.rs
@@ -1,101 +1,53 @@
 use super::Clipboard;
-use winapi::shared::minwindef::TRUE as WIN_TRUE;
-use winapi::shared::ntdef::HANDLE;
-use winapi::shared::windef::HBITMAP;
 use winapi::um::wingdi::*;
 use winapi::um::winuser::*;
 
 pub(crate) struct DIB {
-    width: u32,
-    height: u32,
-    data: Vec<[u8; 4]>,
+    pub head: BITMAPFILEHEADER,
+    pub info: BITMAPINFOHEADER,
+    pub data: Box<[u8]>,
 }
 
 impl DIB {
-    pub unsafe fn get_bmp(clipboard: &mut Clipboard) -> Option<std::io::Result<Self>> {
-        if IsClipboardFormatAvailable(CF_BITMAP) != WIN_TRUE {
-            return None;
-        }
-        let hdl = GetClipboardData(CF_BITMAP);
-        Some(|| -> std::io::Result<Self> {
-            if hdl == std::ptr::null_mut() {
-                return Err(std::io::Error::last_os_error());
-            }
-            let mut hdr = BITMAP::default();
-            if GetObjectW(
-                hdl as HANDLE,
-                std::mem::size_of::<BITMAP> as i32,
-                &mut hdr as *mut BITMAP as *mut std::ffi::c_void,
-            ) == 0
-            {
-                return Err(std::io::Error::last_os_error());
-            }
-            let mut dib = {
-                let (width, height) = (hdr.bmWidth as u32, hdr.bmHeight as u32);
-                Self {
-                    width: width,
-                    height: height,
-                    data: vec![[0u8; 4]; (width * height) as usize],
-                }
-            };
-            if GetBitmapBits(
-                hdl as HBITMAP,
-                (dib.data.capacity() * 4) as i32,
-                dib.data.as_mut_ptr() as *mut std::ffi::c_void,
-            ) == 0
-            {
-                return Err(std::io::Error::last_os_error());
-            }
-            for px in dib.data.iter_mut() {
-                *px = [px[2], px[1], px[0], px[3]];
-            }
-            Ok(dib)
-        }())
-    }
-
-    pub unsafe fn get_clip(clipboard: &mut Clipboard) -> Option<std::io::Result<Self>> {
+    pub unsafe fn unclip(clipboard: &mut Clipboard) -> Option<std::io::Result<Self>> {
         clipboard.get(CF_DIB).map(|gptr| {
             let lock = gptr?;
             let info = lock.as_ref::<BITMAPINFO>();
-            let hdr = info.bmiHeader;
-            let (width, height) = (hdr.biWidth as u32, hdr.biHeight as u32);
-            println!(
-                "image compression = {}",
-                match hdr.biCompression {
-                    BI_JPEG => "jpeg",
-                    BI_PNG => "png",
-                    _ => "misc.",
+            let head = {
+                let offset = {
+                    std::mem::size_of::<BITMAPFILEHEADER>()
+                        + std::mem::size_of::<BITMAPINFOHEADER>()
+                } as u32;
+                BITMAPFILEHEADER {
+                    bfType: 0x4D42, // 'BM'
+                    bfReserved1: 0,
+                    bfReserved2: 0,
+                    bfSize: info.bmiHeader.biSizeImage + offset,
+                    bfOffBits: offset as u32,
                 }
-            );
-            let src = {
-                let ptr = (info as *const BITMAPINFO).offset(1) as *const [u8; 4];
-                let slc = std::slice::from_raw_parts(ptr, (hdr.biSizeImage / 4) as usize);
-                slc.iter().map(|px| [px[2], px[1], px[0], px[3]])
             };
-            let mut dib = Self {
-                width: width,
-                height: height,
-                data: Vec::with_capacity((width * height) as usize),
-            };
-            dib.data.extend(src);
-            Ok(dib)
+            let data = Box::<_>::from({
+                let ptr = (&info.bmiHeader as *const BITMAPINFOHEADER).offset(1) as *const u8;
+                let len = info.bmiHeader.biSizeImage as usize;
+                std::slice::from_raw_parts(ptr, len)
+            });
+            let info = info.bmiHeader;
+            Ok(Self { head, info, data })
         })
     }
 
-    pub fn encode_png<W: std::io::Write>(&self, dst: W) -> std::io::Result<()> {
-        let mut enc = png::Encoder::new(dst, self.width, self.height);
-        enc.set_color(png::ColorType::RGBA);
-        enc.set_depth(png::BitDepth::Eight);
-        let mut steno = enc
-            .write_header()
-            .map_err(|err| -> std::io::Error { err.into() })?;
-        steno
-            .write_image_data(unsafe {
-                let ptr = self.data.as_ptr() as *const u8;
-                let len = self.data.len() * 4;
-                std::slice::from_raw_parts(ptr, len)
-            })
-            .map_err(|err| -> std::io::Error { err.into() })?;
+    pub unsafe fn encode_to<O: std::io::Write>(&self, ostrm: &mut O) -> std::io::Result<()> {
+        ostrm.write({
+            let ptr = <*const _>::cast(&self.head);
+            let len = std::mem::size_of::<BITMAPFILEHEADER>();
+            std::slice::from_raw_parts(ptr, len)
+        })?;
+        ostrm.write({
+            let ptr = <*const _>::cast(&self.info);
+            let len = std::mem::size_of::<BITMAPINFOHEADER>();
+            std::slice::from_raw_parts(ptr, len)
+        })?;
+        ostrm.write(&self.data)?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ mod pin;
 use dib::DIB;
 use pin::*;
 use std::fs::File;
-use std::io::Write;
-use winapi::um::wingdi::*;
 use winapi::um::winuser::*;
 
 fn main() -> std::io::Result<()> {
@@ -25,49 +23,12 @@ fn main() -> std::io::Result<()> {
                 if msg.msg != WM_CLIPBOARDUPDATE {
                     continue;
                 }
-                let clipboard = Clipboard::open()?;
-                if let Some(gptr) = clipboard.get(CF_DIB) {
-                    let lock = gptr?;
-                    let info = lock.as_ref::<BITMAPINFO>();
-                    let file_info = &info.bmiHeader;
-                    let file_hdr = {
-                        let offset = {
-                            std::mem::size_of::<BITMAPFILEHEADER>()
-                                + std::mem::size_of::<BITMAPINFOHEADER>()
-                        } as u32;
-                        BITMAPFILEHEADER {
-                            bfType: 0x4D42,
-                            bfReserved1: 0,
-                            bfReserved2: 0,
-                            bfSize: file_info.biSizeImage + offset,
-                            bfOffBits: offset as u32,
-                        }
-                    };
+                let mut clipboard = Clipboard::open()?;
+                if let Some(result) = DIB::unclip(&mut clipboard) {
+                    let dib = result?;
                     let mut ostrm = File::create("clip.bmp")?;
-                    println!("{:?}", file_hdr);
-                    println!("{:?}", file_info);
-                    ostrm.write({
-                        let ptr = <*const _>::cast(&file_hdr);
-                        let len = std::mem::size_of::<BITMAPFILEHEADER>();
-                        std::slice::from_raw_parts(ptr, len)
-                    })?;
-                    ostrm.write({
-                        let ptr = <*const _>::cast(file_info);
-                        let len = std::mem::size_of::<BITMAPINFOHEADER>();
-                        std::slice::from_raw_parts(ptr, len)
-                    })?;
-                    ostrm.write({
-                        let ptr = (file_info as *const BITMAPINFOHEADER).offset(1) as *const u8;
-                        let len = file_info.biSizeImage as usize;
-                        std::slice::from_raw_parts(ptr, len)
-                    })?;
+                    dib.encode_to(&mut ostrm)?;
                 }
-                // let mut clipboard = Clipboard::open()?;
-                // if let Some(result) = DIB::get_clip(&mut clipboard) {
-                //     let dib = result?;
-                //     let ostrm = File::create("clip.png")?;
-                //     dib.encode_png(ostrm)?;
-                // }
             }
             listener.poll()?;
         }
